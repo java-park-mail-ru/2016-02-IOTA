099c304fa8c2d52415360edcc5f6eed97d0e40aa
Maxim Galaganov <voltage@lanport.net>
2016-05-25 02:18:44 +0300

    Impl. foundation classes for game mechanics

--- src/main/java/su/iota/backend/game/impl/GameMechanicsImpl.java
+++ src/main/java/su/iota/backend/game/impl/GameMechanicsImpl.java
@@ -2,17 +2,27 @@ package su.iota.backend.game.impl;
 
 import co.paralleluniverse.actors.behaviors.ProxyServerActor;
 import org.glassfish.hk2.api.PerLookup;
+import org.jetbrains.annotations.NotNull;
 import org.jvnet.hk2.annotations.Service;
 import su.iota.backend.game.GameMechanics;
+import su.iota.backend.models.game.Coordinate;
+import su.iota.backend.models.game.Field;
+import su.iota.backend.models.game.FieldCell;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Predicate;
 
 @Service
 @PerLookup
 public class GameMechanicsImpl extends ProxyServerActor implements GameMechanics {
 
-
     public GameMechanicsImpl() {
         super(true);
     }
+
+    private Field field = new Field();
 
     //
 
---
+++ src/main/java/su/iota/backend/models/game/Coordinate.java
@@ -0,0 +1,63 @@
+package su.iota.backend.models.game;
+
+import org.jetbrains.annotations.NotNull;
+
+public class Coordinate {
+
+    private final int offX;
+    private final int offY;
+
+    public static final int BASE_X = 34 / 2;
+    public static final int BASE_Y = 34 / 2;
+
+    public Coordinate(int offX, int offY) {
+        this.offX = offX;
+        this.offY = offY;
+    }
+
+    public int getOffX() {
+        return offX;
+    }
+
+    public int getOffY() {
+        return offY;
+    }
+
+    public int getX() {
+        return BASE_X + offX;
+    }
+
+    public int getY() {
+        return BASE_Y + offY;
+    }
+
+    @NotNull
+    public Coordinate plus(@NotNull Coordinate other) {
+        return new Coordinate(offX + other.offX, offY + other.offY);
+    }
+
+    @NotNull
+    public Coordinate minus(@NotNull Coordinate other) {
+        return new Coordinate(offX - other.offX, offY - other.offY);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        final Coordinate other = (Coordinate) obj;
+        return offX == other.offX && offY == other.offY;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = offX;
+        result = 31 * result + offY;
+        return result;
+    }
+
+}
