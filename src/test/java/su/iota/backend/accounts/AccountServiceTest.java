14ed79981ddacb08ffc69f9ab37f822b3f3704ff
Artem Ustimov <art@ustimov.org>
2016-05-25 10:11:41 +0300

    More unit tests for AccountService

--- src/test/java/su/iota/backend/accounts/AccountServiceTest.java
+++ src/test/java/su/iota/backend/accounts/AccountServiceTest.java
@@ -1,11 +1,13 @@
 package su.iota.backend.accounts;
 
 import org.jvnet.testing.hk2testng.HK2;
+import org.omg.CORBA.StringHolder;
 import org.omg.CORBA.portable.Streamable;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import su.iota.backend.accounts.exceptions.UserAlreadyExistsException;
 import su.iota.backend.accounts.impl.AccountServiceMapImpl;
 import su.iota.backend.models.UserProfile;
 
@@ -19,7 +21,11 @@ public class AccountServiceTest {
 
     private final String login = "Ustimov";
     private final String password = "Qwerty123!";
-    private UserProfile userProfile = new UserProfile();
+    private final String email = "art@ustimov.org";
+    private final String login2 = "admin1";
+    private final String password2 = "admin1";
+    private final String email2 = "admin1@mail.ru";
+    private UserProfile userProfile = new UserProfile(login, email, password);
     private long userId;
 
     @Inject
@@ -33,9 +39,6 @@ public class AccountServiceTest {
 
     @BeforeMethod
     public void setUp() throws Exception {
-        userProfile.setLogin(login);
-        userProfile.setPassword(password);
-        userProfile.setEmail("art@ustimov.org");
         userId = accountService.createUser(userProfile);
     }
 
@@ -44,6 +47,160 @@ public class AccountServiceTest {
         if (accountService.isUserExistent(userId)) {
             accountService.deleteUser(userId);
         }
+        if (accountService.isUserExistent(login2)) {
+            final Long id = accountService.getUserId(login2);
+            if (id == null) {
+                return;
+            }
+            accountService.deleteUser(id);
+        }
+    }
+
+    @Test(expectedExceptions = UserAlreadyExistsException.class)
+    public void testCreateUserWithUsedEmail() throws Exception {
+        final UserProfile newUserProfile = new UserProfile(login2, email, password2);
+        accountService.createUser(newUserProfile);
+    }
+
+    @Test(expectedExceptions = UserAlreadyExistsException.class)
+    public void testCreateUserWithUsedLogin() throws Exception {
+        final UserProfile newUserProfile = new UserProfile(login, email2, password2);
+        accountService.createUser(newUserProfile);
+    }
+
+    @Test(expectedExceptions = UserAlreadyExistsException.class)
+    public void testRepeatedCreateUser() throws Exception {
+        accountService.createUser(userProfile);
+    }
+
+    // Todo: specify exception type
+    @Test(expectedExceptions = Exception.class)
+    public void testCreateUserWithNullData() throws Exception {
+        final UserProfile newUserProfile = new UserProfile();
+        accountService.createUser(newUserProfile);
+    }
+
+    // Todo: specify exception type
+    @Test(expectedExceptions = Exception.class)
+    public void testCreateUserWithEmptyData() throws Exception {
+        final UserProfile newUserProfile = new UserProfile("", "", "");
+        accountService.createUser(newUserProfile);
+    }
+
+    // Todo: specify exception type
+    @Test(expectedExceptions = Exception.class)
+    public void testCreateUserWithEmptyLogin() throws Exception {
+        final UserProfile newUserProfile = new UserProfile("", email2, password2);
+        accountService.createUser(newUserProfile);
+    }
+
+    // Todo: specify exception type
+    @Test(expectedExceptions = Exception.class)
+    public void testCreateUserWithNullLogin() throws Exception {
+        final UserProfile newUserProfile = new UserProfile(null, email2, password2);
+        accountService.createUser(newUserProfile);
+    }
+
+    // Todo: specify exception type
+    @Test(expectedExceptions = Exception.class)
+    public void testCreateUserWithEmptyEmail() throws Exception {
+        final UserProfile newUserProfile = new UserProfile(login2, "", password2);
+        accountService.createUser(newUserProfile);
+    }
+
+    // Todo: specify exception type
+    @Test(expectedExceptions = Exception.class)
+    public void testCreateUserWithNullEmail() throws Exception {
+        final UserProfile newUserProfile = new UserProfile(login2, null, password2);
+        accountService.createUser(newUserProfile);
+    }
+
+    // Todo: specify exception type
+    @Test(expectedExceptions = Exception.class)
+    public void testCreateUserWithEmptyPassword() throws Exception {
+        final UserProfile newUserProfile = new UserProfile(login2, email2, "");
+        accountService.createUser(newUserProfile);
+    }
+
+    // Todo: specify exception type
+    @Test(expectedExceptions = Exception.class)
+    public void testCreateUserWithNullPassword() throws Exception {
+        final UserProfile newUserProfile = new UserProfile(login2, email2, null);
+        accountService.createUser(newUserProfile);
+    }
+
+    // Todo: specify exception type
+    @Test(expectedExceptions = Exception.class)
+    public void testCreateUserWithBadEmail() throws Exception {
+        final UserProfile newUserProfile = new UserProfile(login2, "email", password2); // No @
+        accountService.createUser(newUserProfile);
+    }
+
+    // Todo: specify exception type
+    @Test(expectedExceptions = Exception.class)
+    public void testCreateUserWithSpaceInLogin() throws Exception {
+        final UserProfile newUserProfile = new UserProfile("some login", email2, password2);
+        accountService.createUser(newUserProfile);
+    }
+
+    // Todo: specify exception type
+    @Test(expectedExceptions = Exception.class)
+    public void testCreateUserWithShortLogin() throws Exception {
+        final UserProfile newUserProfile = new UserProfile("login", email2, password2); // Min length is 6
+        accountService.createUser(newUserProfile);
+    }
+
+    // Todo: specify exception type
+    @Test(expectedExceptions = Exception.class)
+    public void testCreateUserWithWithBadLogin() throws Exception {
+        final UserProfile newUserProfile = new UserProfile("логин!#@", email2, password2); // /^[a-z0-9]+$/i
+        accountService.createUser(newUserProfile);
+    }
+
+    // Todo: specify exception type
+    @Test(expectedExceptions = Exception.class)
+    public void testCreateUserWithShortPassword() throws Exception {
+        final UserProfile newUserProfile = new UserProfile(login2, email2, "pwd"); // Min length is 6
+        accountService.createUser(newUserProfile);
+    }
+
+    // Todo: specify exception type
+    @Test(expectedExceptions = Exception.class)
+    public void testCreateUserWithBadPassword() throws Exception {
+        final UserProfile newUserProfile = new UserProfile(login2, email2, "пароль!#@"); // /^[a-z0-9]+$/i
+        accountService.createUser(newUserProfile);
+    }
+
+    @Test
+    public void testEditUser() throws Exception {
+        final UserProfile newUserProfile = new UserProfile(login2, email2, password2);
+        accountService.editUser(userId, newUserProfile);
+        final UserProfile updatedUserProfile = accountService.getUserProfile(userId);
+        assertEquals(newUserProfile, updatedUserProfile);
+    }
+
+    @Test
+    public void testUpdateEmail() throws Exception {
+        final UserProfile newUserProfile = new UserProfile(login, email2, password);
+        accountService.editUser(userId, newUserProfile);
+        final UserProfile updatedUserProfile = accountService.getUserProfile(userId);
+        assertEquals(newUserProfile, updatedUserProfile);
+    }
+
+    @Test
+    public void testUpdatePassword() throws Exception {
+        final UserProfile newUserProfile = new UserProfile(login, email, password2);
+        accountService.editUser(userId, newUserProfile);
+        final UserProfile updatedUserProfile = accountService.getUserProfile(userId);
+        assertEquals(newUserProfile, updatedUserProfile);
+    }
+
+    @Test
+    public void testUpdateLogin() throws Exception {
+        final UserProfile newUserProfile = new UserProfile(login2, email, password);
+        accountService.editUser(userId, newUserProfile);
+        final UserProfile updatedUserProfile = accountService.getUserProfile(userId);
+        assertEquals(newUserProfile, updatedUserProfile);
     }
 
     @Test
