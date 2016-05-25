bba3dcf829a45bd283ca9eb1c709e427bd9793d0
Artem Ustimov <art@ustimov.org>
2016-05-25 09:14:20 +0300

    AccountService unit tests

--- src/test/java/su/iota/backend/accounts/AccountServiceTest.java
+++ src/test/java/su/iota/backend/accounts/AccountServiceTest.java
@@ -1,8 +1,13 @@
 package su.iota.backend.accounts;
 
 import org.jvnet.testing.hk2testng.HK2;
+import org.omg.CORBA.portable.Streamable;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import su.iota.backend.accounts.impl.AccountServiceMapImpl;
+import su.iota.backend.models.UserProfile;
 
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -11,6 +16,11 @@ import static org.testng.Assert.*;
 
 @HK2
 public class AccountServiceTest {
+
+    private final String login = "Ustimov";
+    private final String password = "Qwerty123!";
+    private UserProfile userProfile = new UserProfile();
+    private long userId;
 
     @Inject
     @Named("AccountServiceMapImpl")
@@ -21,49 +31,59 @@ public class AccountServiceTest {
         assertEquals(accountService.getClass(), AccountServiceMapImpl.class);
     }
 
-    @Test
-    public void testCreateUser() throws Exception {
-        //
+    @BeforeMethod
+    public void setUp() throws Exception {
+        userProfile.setLogin(login);
+        userProfile.setPassword(password);
+        userProfile.setEmail("art@ustimov.org");
+        userId = accountService.createUser(userProfile);
     }
 
-    @Test
-    public void testEditUser() throws Exception {
-        //
+    @AfterMethod
+    public void tearDown() throws Exception{
+        if (accountService.isUserExistent(userId)) {
+            accountService.deleteUser(userId);
+        }
     }
 
     @Test
     public void testDeleteUser() throws Exception {
-        //
+        accountService.deleteUser(userId);
+        assertFalse(accountService.isUserExistent(userId));
     }
 
     @Test
     public void testGetUserId() throws Exception {
-        //
+        final Long userIdByLogin = accountService.getUserId(login);
+        if (userIdByLogin != null) {
+            assertEquals((long) userIdByLogin, userId);
+        }
     }
 
     @Test
     public void testGetUserProfileByLogin() throws Exception {
-        //
+        final UserProfile userProfileByLogin = accountService.getUserProfile(login);
+        assertEquals(userProfile, userProfileByLogin);
     }
 
     @Test
     public void testGetUserProfileById() throws Exception {
-        //
+        final UserProfile userProfileById = accountService.getUserProfile(userId);
+        assertEquals(userProfile, userProfileById);
     }
 
     @Test
     public void testIsUserPasswordCorrect() throws Exception {
-        //
+        assertTrue(accountService.isUserPasswordCorrect(userId, password));
     }
 
     @Test
     public void testIsUserExistentByLogin() throws Exception {
-        //
+        assertTrue(accountService.isUserExistent(login));
     }
 
     @Test
     public void testIsUserExistentById() throws Exception {
-        //
+        assertTrue(accountService.isUserExistent(userId));
     }
-
 }
